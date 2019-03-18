9ca59f23e2f4f0334208f13dd7f4c222612dabb0
Roger <roger.kalliomaki@claremont.se>
2019-03-18 13:26:31 +0100

    added plot_cvs function

--- analyse.py
+++ analyse.py
@@ -1,12 +1,89 @@
 import os
 import preprocessing as pp
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.manifold import TSNE
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import matplotlib.cm as cm
+import numpy as np
 
-STOPWORD_PATHS = ['./data/stopwords_sv.txt']
 
 
-for subdir, dirs, files in os.walk('./data/CVs'):
-	for file in files:
-		filepath = subdir + os.sep + file
-		print(subdir[11:14])
-		
-print(pp.read_word_file('./data/CVs/CIM/Svenska/CV_Claremont_Roger_Kalliomäki.docx'))
+def plot_cvs(cv_path, stopwords):
+	"""
+	Läser in alla word-filer som finns i mappen "cv_path" och visualiserar datan
+	genom att göra en dimensionell reduktion till 2D och plotta både k-means kluster
+	och alla bolag för sig. Argumentet "stopwords" ska vara en lista med sökvägar till
+	text dokument innehållande de stoppord som önskas användas.
+	"""
+	
+	# Läser in word filer och städar upp med angivna stoppord.
+	cv_list = []
+	lbl_indx = []
+	lbl_cntr = -1
+	dir_path = ''
+	for subdir, dirs, files in os.walk(cv_path):
+		for file in files:
+			cv_filepath = subdir + os.sep + file
+			if dir_path != subdir:
+				lbl_cntr += 1
+			cleaned_cv = pp.clean_text(pp.read_word_file(cv_filepath), stopwords)
+			cv_list.append(cleaned_cv)
+			lbl_indx.append(lbl_cntr)
+			dir_path = subdir
+
+	# Skapar en TF-IDF vektor
+	tfidf = TfidfVectorizer()
+	tfidf_vector = tfidf.fit_transform(cv_list)
+
+	# Hitta kluster med k-means
+	km = KMeans(n_clusters=17, init='k-means++', max_iter=100, n_init=5, verbose=1)
+	km.fit(tfidf_vector)
+
+	# Dimensionell reduktion till 2D
+	tfs_reduced = TruncatedSVD(n_components=17, random_state=0).fit_transform(tfidf_vector)
+	tfs_embedded = TSNE(n_components=2, perplexity=40, verbose=2).fit_transform(tfs_reduced)
+
+	# Plotta kluster
+	fig = plt.figure(figsize = (10, 10))
+	ax1 = plt.axes()
+	plt.scatter(tfs_embedded[:, 0], tfs_embedded[:, 1], marker = "x", c = km.labels_)
+	plt.show()
+	plt.close()
+
+	# Plotta olika bolag
+	labels = ['CA', 'CAB', 'CAD', 'CBD', 'CBS', 'CBT', 'CDB', 'CDS', 'CEC', 'CED', 'CIM', 'CLD', 'CNP', 'CNS', 'CQM', 'CQS', 'CXD']
+	markers = ['o', 'v', '^', '<', '>', 's', 'p', '*', '+', 'x', 'X', 'D', '1', '2', '3', '4', '_']
+	colors = cm.rainbow(np.linspace(0, 1, 18))
+	fig = plt.figure(figsize = (10, 10))
+	ax1 = plt.axes()
+	lbl = labels[0]
+	ax1.scatter(tfs_embedded[0, 0], tfs_embedded[0, 1], marker = markers[0], color = colors[lbl_indx[0]], label = lbl)
+	for indx in range(1,len(tfs_embedded[:, 0])):
+		if lbl == labels[lbl_indx[indx]]:
+			ax1.scatter(tfs_embedded[indx, 0], tfs_embedded[indx, 1], marker = markers[lbl_indx[indx]], color = colors[lbl_indx[indx]])
+
+		else:
+			lbl = labels[lbl_indx[indx]]
+			ax1.scatter(tfs_embedded[indx, 0], tfs_embedded[indx, 1], marker = markers[lbl_indx[indx]], color = colors[lbl_indx[indx]], label = lbl)
+	ax1.legend()
+	plt.show()
+
+
+
+
+if __name__ == "__main__":
+	plot_cvs('./data/CVs/Svenska', ['./data/stopwords_sv.txt'])
+
+
+
+#print(pp.read_word_file('./data/CVs/CIM/Svenska/CV_Claremont_Roger_Kalliomäki.docx'))
+
+
+
+
+
+
+
+
